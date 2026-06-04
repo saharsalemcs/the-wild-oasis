753dfd39d21b4eb08da947866cd8a30945540c2b
saharsalemcs <saharsalemcs@gmail.com>
2026-06-05 00:58:38 +0300

    implement dark mode with css variables

---
+++ src/context/DarkModeContext.jsx
@@ -0,0 +1,34 @@
+import { createContext, useContext, useEffect } from "react";
+import { useLocalStorageState } from "../hooks/useLocalStorageState";
+
+const DarkModeContext = createContext();
+
+function DarkModeProvider({ children }) {
+  const [isDarkMode, setIsDarkMode] = useLocalStorageState(false, "isDarkMode");
+
+  useEffect(
+    function () {
+      document.documentElement.classList.toggle("dark-mode");
+    },
+    [isDarkMode],
+  );
+
+  function toggleDarkMode() {
+    setIsDarkMode((isDark) => !isDark);
+  }
+  return (
+    <DarkModeContext.Provider value={{ isDarkMode, toggleDarkMode }}>
+      {children}
+    </DarkModeContext.Provider>
+  );
+}
+
+function useDarkMode() {
+  const context = useContext(DarkModeContext);
+  if (context === undefined)
+    throw new Error("DarkModeContext is used outside the context provider");
+
+  return context;
+}
+
+export { useDarkMode, DarkModeProvider };
