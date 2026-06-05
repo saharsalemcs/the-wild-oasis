a4fd6b1d52ec2e3cd3936b3a50146991632bfaeb
saharsalemcs <saharsalemcs@gmail.com>
2026-06-05 16:47:40 +0300

    building dashboard layout & computing recent bookings and stays

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,4 +1,7 @@
 import styled from "styled-components";
+import { useRecentBookings } from "./useRecentBookings";
+import { useRecentStays } from "./useRecentStays";
+import Spinner from "../../ui/Spinner";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -6,3 +9,14 @@ const StyledDashboardLayout = styled.div`
   grid-template-rows: auto 34rem auto;
   gap: 2.4rem;
 `;
+
+function DashboardLayout() {
+  const { bookings, isLoading: isLoadingBookings } = useRecentBookings();
+  const { isLoading: isloadingStays, stays, confirmedStays } = useRecentStays();
+
+  if (isLoadingBookings || isloadingStays) return <Spinner />;
+
+  return <StyledDashboardLayout></StyledDashboardLayout>;
+}
+
+export default DashboardLayout;
